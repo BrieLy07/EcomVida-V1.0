a09c0d242fa06ce5870783d7284d67addfc53e97
Gabriel Mendoza <gabriel1604@hotmail.es>
2025-06-24 11:18:26 -0500

    Deploy User-Profile AWS

--- autenticacion/user-profile-service/src/database.py
+++ autenticacion/user-profile-service/src/database.py
@@ -2,11 +2,16 @@ import os
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
+# Cargar variables de entorno
 load_dotenv()
 
+# Obtener valores del .env
 mongo_uri = os.getenv("MONGO_URI")
-db_name = os.getenv("DB_NAME")
+db_name = os.getenv("DB_NAME", "perfilusuarios")  # Por defecto usamos "perfilusuarios"
 
+# Conectar a MongoDB Atlas
 client = MongoClient(mongo_uri)
+
+# Seleccionar base de datos y colección
 db = client[db_name]
-coleccion_perfiles = db['perfiles']
+coleccion_perfiles = db["perfiles"]
