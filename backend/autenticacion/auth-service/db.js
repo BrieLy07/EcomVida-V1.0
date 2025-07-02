5a384dc8664bd9cc932375b1459f1877e35903be
Gabriel Mendoza <gabriel1604@hotmail.es>
2025-07-02 17:58:54 -0500

    Fix: activar conexión SSL con RDS PostgreSQL

--- backend/autenticacion/auth-service/db.js
+++ backend/autenticacion/auth-service/db.js
@@ -8,6 +8,9 @@ const pool = new Pool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   port: 5432,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 const initDB = async () => {
