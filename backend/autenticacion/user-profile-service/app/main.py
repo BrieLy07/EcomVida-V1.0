c2300aa0f3603c76673bf18103aef3e332339c84
Gabriel Mendoza <gabriel1604@hotmail.es>
2025-07-03 17:50:26 -0500

    Arreglo de Cod Profile Service

--- backend/autenticacion/user-profile-service/app/main.py
+++ backend/autenticacion/user-profile-service/app/main.py
@@ -1,14 +1,12 @@
 from fastapi import FastAPI
 from app.routes.obtener_usuario import router as obtener_router
 from app.routes.actualizar_usuario import router as actualizar_router
-from app.routes import create_user 
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(obtener_router)
 app.include_router(actualizar_router)
-app.include_router(create_user.router)
 
 app.add_middleware(
     CORSMiddleware,
