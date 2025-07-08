cde9b1091f1355d143719e03325c74b537a689da
Gabriel Mendoza <gabriel1604@hotmail.es>
2025-07-08 11:33:00 -0500

    Arreglo de CORS en Address_Service

--- backend/autenticacion/address-service/main.go
+++ backend/autenticacion/address-service/main.go
@@ -26,5 +26,21 @@ func main() {
 	}
 
 	log.Println("Servidor escuchando en el puerto " + port)
-	log.Fatal(http.ListenAndServe(":"+port, r))
+	log.Fatal(http.ListenAndServe(":"+port, habilitarCORS(r)))
+
+}
+
+// MIDDLEWARE CORS
+func habilitarCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Permitir solo el origen del frontend
+		w.Header().Set("Access-Control-Allow-Origin", "http://ecomvida-alb-973389764.us-east-1.elb.amazonaws.com")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		h.ServeHTTP(w, r)
+	})
 }
